912d1fa6a4d3fc0c2c64b6c947ac237fa73a5f23
mwvalen <mwillvalentine@gmail.com>
2018-12-30 12:57:36 -0500

    Add project modal & functionality implemented

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,27 +1,14 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux';
+import slugify from 'slugify';
+import _ from 'lodash';
+import colors from '../../utils/colorList'
 import { addProject } from '../../actions/manageProjects'
 import './styles.scss'
 
+
 const sidebarProjectsSort = (a, b) => {
     return a.name < b.name ? -1 : 1
-}
-
-const AddProjectButton = ({onAddProject, updateNewProjectText, projectText}) => {
-    return (
-        <div>
-            <form onSubmit={onAddProject}>
-                <input type="text" 
-                        name="projectText" 
-                        onChange={updateNewProjectText} 
-                        value={projectText}
-                />
-                <button type="submit">
-                    Add Project
-                </button>
-            </form>
-        </div>
-    )
 }
 
 const Sidebar = ({projects}) => (
@@ -31,7 +18,7 @@ const Sidebar = ({projects}) => (
             { 
                 projects.sort(sidebarProjectsSort).map(p => (
                     <li className="nav-item">
-                        <a class="nav-link" href="#">
+                        <a className="nav-link" href="#">
                             {p.name}
                         </a>
                     </li>
@@ -41,33 +28,137 @@ const Sidebar = ({projects}) => (
     </div>
 )
 
+const AddProjectButton = ({onAddProject, errorText, updateNewProjectText, projectText}) => {
+    
+    const inputKeyPress = e => {
+        if (e.key === "Enter") onAddProject()
+    }
+    const focusInput = e => {
+        window.setTimeout(function ()
+            {
+                document.getElementById('projectNameInput').focus();
+            }, 0);
+    }
+    return (
+        <div className='d-inline-block'>
+        <button type="button" className="btn btn-outline-primary" data-toggle="modal" data-target="#addProjectModal" onClick={focusInput}   >
+            Add New Project
+        </button> 
+        <div className="modal fade" id="addProjectModal" role="dialog" aria-labelledby="addNewProject" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Add New Project</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <label htmlFor="projectTitleInput control-label">Project Name</label>
+                            <input  
+                                className="form-control"
+                                type="text" 
+                                name="projectText" 
+                                onChange={updateNewProjectText} 
+                                value={projectText}
+                                onKeyPress={inputKeyPress}
+                                id="projectNameInput"
+                            />
+                            <div className="errorText pt-2">{errorText}</div>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal" id="closeModal">Close</button>
+                            <button type="button" className="btn btn-primary" onClick={onAddProject}>Add Project</button>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        </div>
+    )
+}
+
 class Dashboard extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            newProjectText: ''
+            newProjectText: '',
+            addProjectErrorText: ''
         }
     }
     updateNewProjectText = e => {
         this.setState({...this.state, newProjectText: e.target.value})
     }
+    validateNewProject = () => {
+        if (this.state.newProjectText.length === 0) {
+            this.setState({...this.state, addProjectErrorText: 'Project Name Can Not Be Blank'})
+            return false
+        }
+        if (!RegExp(/^[a-z0-9 ]+$/i).test(this.state.newProjectText)) {
+            this.setState({...this.state, addProjectErrorText: 'Project Name Must Only Contain Alphanumeric Characters'})
+            return false
+        }
+        if (this.props.projects.filter(p => {   //use filter because IE doesn't support find
+            return p.name.toLowerCase() === this.state.newProjectText.trim()
+            })[0]) 
+        {
+            this.setState({...this.state, addProjectErrorText: 'Project Name Must Be Unique'})
+            return false
+        }
+        if (this.props.projects.length >= 20) {
+            this.setState({...this.state, addProjectErrorText: 'Max 20 Projects'})
+            return false
+        }
+        /*else if (this.props.projects.find((p) => p.name.toLowerCase() === this.state.newProjectText.trim())) {
+            this.setState({...this.state, addProjectErrorText: 'Project Name Must Be Unique'})
+            return false
+        }*/
+        return true
+    }
+    generateProjectColor = () => {
+        let filteredColors = colors.filter(c => {
+            return !_.includes(this.props.projects  //USE LODASH INCLUDES FOR IE (NO SUPPORT FOR NATIVE INCLUDES)
+                .map(p => p.color), c)
+        })
+        let color = filteredColors[Math.floor(Math.random()*filteredColors.length)];
+        return color
+    }
     addProject = e => {
-        e.preventDefault();
-        this.props.addProject({name: this.state.newProjectText})
-        this.setState({...this.state, newProjectText: ''})
+        if (this.validateNewProject()) {
+            this.props.addProject(
+                {
+                    name: this.state.newProjectText, 
+                    slug: slugify(this.state.newProjectText),
+                    color: this.generateProjectColor()
+                })
+            this.setState({...this.state, newProjectText: '', addProjectErrorText: ''})
+            document.getElementById("closeModal").click()
+        }
+    }
+    setProjectModalOpen = open => {
+        if (this.state.addProjectModalOpen != open) 
+            this.setState({...this.state, addProjectModalOpen: !this.state.addProjectModalOpen})
     }
     render() {
         const {projects} = this.props
+        console.log(projects)
         return (    
             <div className="row main-row">  
                 <div className="col-md-2 bg-light sidebar projects-sidebar h-100">
                     <Sidebar projects={this.props.projects} />
                 </div>
                 <div className="col-md-10 pt-2">
-                    <AddProjectButton onAddProject={this.addProject}
-                                    updateNewProjectText={this.updateNewProjectText}
-                                    projectText={this.state.newProjectText}
-                    />
+                    <div className="rowpr-3">
+                        <div className="col-md-12  d-flex  flex-row-reverse">
+                        <AddProjectButton onAddProject={this.addProject.bind(this)}
+                                updateNewProjectText={this.updateNewProjectText.bind(this)}
+                                projectText={this.state.newProjectText}
+                                errorText={this.state.addProjectErrorText} />
+                           
+                        </div>
+                    </div>
+                    <div className="row">
+                    </div>
+                    
                 </div>
             </div>
         )
